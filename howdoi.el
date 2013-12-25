4afafaf02b240f1472a121e103852d217be342e6
Arthur Neves <arthurnn@gmail.com>
2013-12-25 16:16:22 -0500

    howdoi.el file

---
+++ howdoi.el
@@ -0,0 +1,32 @@
+;;; howdoi.el --- Emacs inlune stackoverflow`s search
+
+;; Copyright © 2013 Arthur Nogueira Neves
+
+;; Author: Arthur Neves
+;; URL: https://github.com/arthurnn/howdoi-emacs
+;; Version: 0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Code:
+
+(defvar howdoi-program "howdoi")
+
+(defun howdoi--exec-comand (command)
+  (shell-command-on-region
+   (region-beginning)
+   (region-end)
+   command
+   t))
+
+
+;;;###autoload
+(defun howdoi ()
+  "parse csv in php"
+  (interactive)
+  (let ((command (buffer-substring (region-beginning) (region-end))))
+      (howdoi--exec-comand (format "%s %s" howdoi-program command))
+      ))
+
+(provide 'howdoi)
+;;; howdoi.el ends here
