3e95e148d8bc1de37ad90f52a4ce9092e937f120
Steve Purcell <steve@sanityinc.com>
2014-01-18 11:03:26 +0000

    Various fixes
    * Use "r" with 'interactive to get region extents
    * Use standard header
    * Use defgroup/defcustom
    * Inline "exec-command" function

--- howdoi.el
+++ howdoi.el
@@ -1,32 +1,56 @@
-;;; howdoi.el --- Emacs inlune stackoverflow`s search
+;;; howdoi.el --- Use howdoi program to search Stackoverflow for the selected text
 
-;; Copyright © 2013 Arthur Nogueira Neves
+;; Copyright (C) 2013  Arthur Nogueira Neves
 
 ;; Author: Arthur Neves
 ;; URL: https://github.com/arthurnn/howdoi-emacs
 ;; Version: 0.1
+;; Keywords: tools
 
 ;; This file is NOT part of GNU Emacs.
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Use the howdoi command-line program to search Stackoverflow for
+;; the text in the current region.
+
+;; See https://github.com/gleitz/howdoi
+
 ;;; Code:
 
-(defvar howdoi-program "howdoi")
+(defgroup howdoi nil
+  "Integration with the \"howdoi\" command-line program."
+  :group 'tools)
 
-(defun howdoi--exec-comand (command)
-  (shell-command-on-region
-   (region-beginning)
-   (region-end)
-   command
-   t))
-
+(defcustom howdoi-program "howdoi"
+  "The path to the \"howdoi\" executable."
+  :type 'file
+  :group 'howdoi)
 
 ;;;###autoload
-(defun howdoi ()
-  "How do I? search on stackoverflow"
-  (interactive)
-  (let ((command (buffer-substring (region-beginning) (region-end))))
-      (howdoi--exec-comand (format "%s %s" howdoi-program command))
-      ))
+(defun howdoi (beg end)
+  "Replace the region from BEG to END with the howdoi search results for that text."
+  (interactive "r")
+  (let ((terms (buffer-substring beg end)))
+    (shell-command-on-region
+     beg
+     end
+     (format "%s %s" howdoi-program (shell-quote-argument terms))
+     t)))
+
 
 (provide 'howdoi)
 ;;; howdoi.el ends here
